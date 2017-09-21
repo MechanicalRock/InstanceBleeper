d2fd39662bfbd5fd4f5690dd5527add50a18a0f7
Bryan.Yu <bryan.yu@magentys.io>
2017-09-21 16:18:23 +0800

    remove old yml templates, update to new per-region deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,29 +5,42 @@ declare -a regions=('ap-southeast-1' 'ap-southeast-2' 'us-east-1' 'us-east-2' 'u
 # arguments
 ACCOUNT_ID=$1
 DESTINATION_EMAIL=$2
+STACK_OWNER=$3
 
 pip install awscli --upgrade --user
 
-# package lambda function
+package lambda function
 sudo npm i -g typescript
+rm -rf node_modules
 npm i --only=production
 tsc
-# zip -r instancebleeper.zip src/*.js
+zip -r instancebleeper.zip dist node_modules templates/template.html
 
-# deploy everything
-aws cloudformation create-stack --stack-name cf-stack-set-role \
+# value of stack will be empty string if this command fails
+for region in ${regions[@]}
+do
+	stack=$(aws cloudformation describe-stacks --stack-name cf-instance-bleeper --region "$region" --query 'Stacks[0]')
+	bucket_name="instance-bleeper-$region"
+
+	if [ -z "$stack" ]; then
+		aws cloudformation create-stack --stack-name cf-instance-bleeper \
+		--capabilities CAPABILITY_NAMED_IAM \
+		--template-body file://templates/s3_bucket.yml \
+		--parameters ParameterKey=BucketName,ParameterValue=$bucket_name \
+		--region $region \
+		--tags Key=Owner,Value="${STACK_OWNER}" \
+
+		aws cloudformation wait stack-create-complete --stack-name cf-instance-bleeper --region $region
+	fi
+
+	aws s3 cp ./instancebleeper.zip s3://${bucket_name}/instancebleeper.zip
+
+	aws cloudformation update-stack --stack-name cf-instance-bleeper \
 	--capabilities CAPABILITY_NAMED_IAM \
-	--template-body file://templates/AWSCloudFormationStackSetAdministrationRole.yml --region 'ap-southeast-1'
-
-aws cloudformation create-stack --stack-name cf-stack-set-role-trust \
-	--parameters ParameterKey=AdministratorAccountId,ParameterValue=$ACCOUNT_ID \
-	--capabilities CAPABILITY_NAMED_IAM \
-	--template-body file://templates/AWSCloudFormationStackSetExecutionRole.yml --region 'ap-southeast-1'
-
-aws cloudformation create-stack-set --stack-set-name instance-bleeper \
-	--capabilities CAPABILITY_IAM --template-body file://templates/stack.yaml \
-	--parameters ParameterKey=DestinationEmail,ParameterValue=$DESTINATION_EMAIL --region 'ap-southeast-1'
-
-# aws cloudformation create-stack-instances --stack-set-name instance-bleeper --accounts $ACCOUNT_ID --regions ${regions[@]}
-
-`insert new cloudformation command here`
+	--template-body file://templates/event_handler.yml \
+	--parameters \
+		ParameterKey=DestinationEmail,ParameterValue=$DESTINATION_EMAIL \
+		ParameterKey=BucketName,ParameterValue=$bucket_name \
+	--region $region \
+	--tags Key=Owner,Value="${STACK_OWNER}"
+done
