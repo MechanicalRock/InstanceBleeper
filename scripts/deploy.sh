57e848e7ac3b21ec5504a40d67c8d6163700af46
Bryan.Yu <bryan.yu@magentys.io>
2017-10-13 10:58:18 +0800

    revert code

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# declare -a regions=('ap-southeast-1' 'ap-southeast-2' 'us-east-1' 'us-east-2' 'us-west-1' 'us-west-2' 'ap-northeast-2' 'ap-south-1' 'ap-northeast-1' 'ca-central-1' 'eu-central-1' 'eu-west-1' 'eu-west-2' 'sa-east-1')
-declare -a regions=('ap-southeast-1')
-
+declare -a regions=('ap-southeast-1' 'ap-southeast-2' 'us-east-1' 'us-east-2' 'us-west-1' 'us-west-2' 'ap-northeast-2' 'ap-south-1' 'ap-northeast-1' 'ca-central-1' 'eu-central-1' 'eu-west-1' 'eu-west-2' 'sa-east-1')
 
 # arguments
 ACCOUNT_ID=$1
